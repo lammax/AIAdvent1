753775401f8c934ea25dc4bc4d6580d0bfeb4b46
Lammax <lammax@mail.ru>
2026-03-16 15:16:01 +0300

    Initial Commit

---
+++ AIChallenge/AIChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChallengeApp.swift
+//  AIChallenge
+//
+//  Created by Максим Ламанский on 16.03.26.
+//
+
+import SwiftUI
+
+@main
+struct AIChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChallengeTests/AIChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChallengeTests.swift
+//  AIChallengeTests
+//
+//  Created by Максим Ламанский on 16.03.26.
+//
+
+import Testing
+@testable import AIChallenge
+
+struct AIChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
